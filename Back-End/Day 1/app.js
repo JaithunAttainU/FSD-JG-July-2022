70b06e3413dd9e8be26a6f3a616a37ba4dea5202
jmahirakz <jmahirakz@paypal.com>
2022-11-03 00:02:18 +0530

    Node Intro

---
+++ Back-End/Day 1/app.js
@@ -0,0 +1,10 @@
+const chalk = require('chalk')
+
+console.log(chalk.green('Hello World'))
+console.log(chalk.bgRed("Red Background"))
+// console.log(document.body)
+// console.log(localStorage.clear())
+
+setTimeout(() => {
+  console.log("2 Secs")
+})
